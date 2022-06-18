e717b617245d330a4dd93b53aa15eb7874320268
YsNaive <yi.shi.4589@gmail.com>
2022-06-18 15:00:04 +0800

    Add : event data & front

---
+++ Assets/Scripts/ScriptableObject/EventData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "New Event Data")]
+public class EventData : ScriptableObject
+{
+    public string ID;
+    public int FavorableEffect;
+}
