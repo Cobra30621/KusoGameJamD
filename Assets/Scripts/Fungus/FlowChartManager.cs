243fbb92e5cc8cf0d4abc395623ec6cc8367e652
Cobra30621 <cobra20621@gmail.com>
2022-06-18 14:27:22 +0800

    set floder structure

---
+++ Assets/Scripts/Fungus/FlowChartManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class FlowChartManager : MonoBehaviour
+{
+    public Flowchart m_flowchart;
+    public string testBlock;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    [ContextMenu("Test Block")]
+    public void TestBlock(){
+        PlayBlock(testBlock);
+    }
+
+    public void PlayBlock(string block){
+        UseFungus.PlayBlock( m_flowchart ,block );
+    }
+}
