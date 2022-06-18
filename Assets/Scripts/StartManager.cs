5d138cd617d4e792ad57781173a678ff67cd4b58
Cobra30621 <cobra20621@gmail.com>
2022-06-19 00:03:52 +0800

    Add Start.scene

---
+++ Assets/Scripts/StartManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartManager : MonoBehaviour
+{
+    public GameObject startPanel;
+    public GameObject teachingPanel;
+
+    public void ShowTeachingPanel(){
+        startPanel.SetActive(false);
+        teachingPanel.SetActive(true);
+    }
+
+    public void StartGame(){
+        SceneManager.LoadScene("Main");
+    }
+}
