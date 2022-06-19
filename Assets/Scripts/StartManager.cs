6578ae4e671edc112cf33466d20601a7e1531db7
Cobra30621 <cobra20621@gmail.com>
2022-06-19 16:34:46 +0800

    fix:end save

--- Assets/Scripts/StartManager.cs
+++ Assets/Scripts/StartManager.cs
@@ -5,10 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class StartManager : MonoBehaviour
 {
+    public EndingManager endingManager;
     public GameObject teachingPanel;
     public List<GameObject> closeObj;
     public GameObject start_button; 
     public float duration;
+
+    void Start(){
+        endingManager.ResetSaveData();
+    }
 
     public void ShowTeachingPanel(){
         foreach (GameObject go in closeObj)
