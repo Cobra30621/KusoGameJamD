000b07490f8b25a1b63b424862406a51f9f4eb5e
fantasystarwd <fantasystar951@gmail.com>
2022-06-19 10:48:31 +0800

    add blur effect & breath animation

--- Assets/Scripts/PostEffect/BlurPostEffect.cs
+++ Assets/Scripts/PostEffect/BlurPostEffect.cs
@@ -5,18 +5,6 @@ using UnityEngine;
 public class BlurPostEffect : MonoBehaviour
 {
     public Material mat;
-    public Texture2D noiseTex;
-
-    void Start()
-    {
-        mat.SetTexture("_NoiseTex", noiseTex);
-    }
-
-    void FixedUpdate()
-    {
-        // Scale speed of water droplets
-        mat.SetFloat("_NoiseOffset", Time.time * 0.05f);
-    }
 
     void OnRenderImage(RenderTexture src, RenderTexture dst)
     {
